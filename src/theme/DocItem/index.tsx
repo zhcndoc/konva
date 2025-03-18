05bd5a2954543c36b63a78dee081c15feb2db785
Anton Lavrevov <lavrton@gmail.com>
2025-03-18 13:58:56 -0500

    fix disqus

--- src/theme/DocItem/index.tsx
+++ src/theme/DocItem/index.tsx
@@ -3,17 +3,20 @@ import DocItem from '@theme-original/DocItem';
 import type DocItemType from '@theme/DocItem';
 import type { WrapperProps } from '@docusaurus/types';
 import { DiscussionEmbed } from 'disqus-react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 type Props = WrapperProps<typeof DocItemType>;
 
 export default function DocItemWrapper(props: Props): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+
   return (
     <>
       <DocItem {...props} />
       <DiscussionEmbed
         shortname="konvajs"
         config={{
-          url: props.content.metadata.permalink,
+          url: siteConfig.url + props.content.metadata.permalink,
           identifier: props.content.metadata.permalink,
           title: props.content.metadata.title,
         }}
